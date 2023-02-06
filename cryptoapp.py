f0faca1efcc96a0284d7ea83f6a9a21f5e3507de
vonnkee13 <65433415+vonnkee13@users.noreply.github.com>
2023-02-06 18:18:52 +0800

    Update cryptoapp.py

--- cryptoapp.py
+++ cryptoapp.py
@@ -7,8 +7,8 @@ import datetime
 
 
 # Map crypto to their yfinance tickers
-keys = ['Bitcoin', 'Ethereum', 'Shiba Inu','SLP', 'XRP', 'Axie Infinity']
-values = ['BTC-USD', 'ETH-USD', 'SHIB-USD','SLP-USD', 'XRP-USD', 'AXS-USD']
+keys = ['Bitcoin', 'Ethereum', 'Shiba Inu','Chainlink' 'SLP', 'XRP', 'Axie Infinity']
+values = ['BTC-USD', 'ETH-USD', 'SHIB-USD','LINK-USD','SLP-USD', 'XRP-USD', 'AXS-USD']
 
 # Create the dictionary
 crypto_dict = dict(zip(keys, values))
